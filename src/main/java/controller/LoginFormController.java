91f3e01ea1b33e0402261e25666c5421cfe28631
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-21 22:21:13 +0530

    Implement login form UI and controller

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,56 @@
 package controller;
 
-public class LoginFormController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginFormController implements Initializable {
+
+    public TextField txtUsername;
+    public PasswordField txtPassword;
+
+    Stage stage = new Stage();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        txtUsername.setOnAction(actionEvent -> txtPassword.requestFocus());
+
+        txtPassword.setOnAction(actionEvent -> loginButtonOnAction (new ActionEvent()));
+    }
+
+
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+        String name = "Chamod";
+        String password = "1234";
+
+        if (name.equals(txtUsername.getText()) && password.equals(txtPassword.getText())) {
+            try {
+                txtUsername.setText("");
+                txtPassword.setText("");
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/dashboard.fxml"))));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            stage.show();
+        } else {
+            txtUsername.setText("");
+            txtPassword.setText("");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Error");
+            alert.setContentText("UserName or Password is wrong");
+            alert.show();
+        }
+    }
+
 }
